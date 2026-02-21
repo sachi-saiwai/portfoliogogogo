a38a8e503949760d4856eb98fa22fda4b5bb675b
sachi0408116 <sachikosagagogogo@gmail.com>
2026-02-21 10:17:34 +0900

    トップ画像のレスポンシブ対応を行いました

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
+    return MaterialApp(
       title: 'sachikogogogo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
@@ -46,97 +46,98 @@ class _MyHomePageState extends State<MyHomePage> {
         decoration: BoxDecoration(color: Colors.black),
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              // const Text('私の',
-              // style:TextStyle(
-              //   fontSize:50,
-              //   color: Colors.white,
-              // )),
-              SizedBox(height: 50),
-              Row(
-                children:[
-                      ElevatedButton(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                // const Text('私の',
+                // style:TextStyle(
+                //   fontSize:50,
+                //   color: Colors.white,
+                // )),
+                SizedBox(height: 50),
+                Row(
+                  children: [
+                    ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black,
                           foregroundColor: Colors.black,
-                          ),
+                        ),
                         child: Text('Artworks',
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
                         onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ArtPage())
-                        );
-                      }),
-                      ElevatedButton(
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ArtPage()));
+                        }),
+                    ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black,
                           foregroundColor: Colors.black,
-                          ),
+                        ),
                         child: Text('Creations',
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
                         onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => CreationPage())
-                        );
-                      }),
-                      ElevatedButton(
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CreationPage()));
+                        }),
+                    ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.black,
                           foregroundColor: Colors.black,
-                          ),
+                        ),
                         child: Text('Links',
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
                         onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LinkPage())
-                        );
-                      }),
-                ],
-              ),
-              const Text('My portfolio',
-              style:TextStyle(
-                fontSize:50,
-                color: Colors.white,
-              )
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-             SingleChildScrollView(
-             scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(child:Image.asset('assets/kuma.png'),height:MediaQuery.of(context).size.height * 0.9,width:MediaQuery.of(context).size.width * 0.9,),
-                ]
-                  )
-             ),
-        //      ElevatedButton(
-        //       // onPressed: () {
-        //       //   Navigator.push(
-        //       //   context,
-        //       //   MaterialPageRoute(builder: (context) => ArtPage()),
-        //       //   );
-        //       //   },
-        //         child:Container(
-        //           alignment: Alignment.center,
-        //           child: Text('My artworks'),
-        //           height:60,
-        //           width: 120,
-        //           ),
-        // ),  
-            ]
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LinkPage()));
+                        }),
+                  ],
+                ),
+                const Text('My portfolio',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                    )),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.9,
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: Image.asset('assets/kuma.png',
+                                fit: BoxFit.contain),
+                          ),
+                        ])),
+                //      ElevatedButton(
+                //       // onPressed: () {
+                //       //   Navigator.push(
+                //       //   context,
+                //       //   MaterialPageRoute(builder: (context) => ArtPage()),
+                //       //   );
+                //       //   },
+                //         child:Container(
+                //           alignment: Alignment.center,
+                //           child: Text('My artworks'),
+                //           height:60,
+                //           width: 120,
+                //           ),
+                // ),
+              ]),
         ),
-          ),
       ),
-      );
+    );
   }
 }
